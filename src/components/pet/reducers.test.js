01b5840df1db913ec3e2359e0f622b0d2bab1807
Christina Mills <chrimills@gmail.com>
2018-03-16 12:52:32 -0700

    2 passing tests

--- src/components/pet/reducers.test.js
+++ src/components/pet/reducers.test.js
@@ -1,8 +1,13 @@
 import { ADD_PET, REMOVE_PET, pet } from './reducers';
 
-const petToAdd = { name: 'Jim', animal: 'dog', description: 'standup dog' };
+const petToAdd = { id: 123, name: 'Jim', animal: 'dog', description: 'standup dog' };
 
 it('adds a pet', () => {
   const state = pet([], { type: ADD_PET, payload: petToAdd });
   expect(state).toEqual([petToAdd]);
+});
+
+it('Remove a pet', () => {
+  const state = pet([petToAdd], { type: REMOVE_PET, payload: 123 });
+  expect(state).toEqual([]);
 });
