52c139c417c3028984a5361c8320d0a8b557d5fd
CharlyWelch <blwbiology@gmail.com>
2018-03-16 13:00:08 -0700

    final push to get ids

--- src/components/pets/actions.js
+++ src/components/pets/actions.js
@@ -1,8 +1,9 @@
 import { PET_ADD, PET_DELETE } from './reducers';
-import shortid from 'shortid';
+// import shortid from 'shortid';
+const Hashids = require('hashids');
 
 export function addPet(pet){
-  pet.id = shortid();
+  pet.id = new Hashids(); // error: Cannot create property 'id' on string. with both shortid and hashids
   return {
     type: PET_ADD,
     payload: pet
